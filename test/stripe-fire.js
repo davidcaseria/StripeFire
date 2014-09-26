d5e52db36dd5b32c4d75f73932a572483e2221f4
Dave <davidcaseria@gmail.com>
2014-09-25 21:02:15 -0500

    updated test script

--- test/stripe-fire.js
+++ test/stripe-fire.js
@@ -1,10 +1,39 @@
-var should = require('should');
+var Firebase = require('firebase'),
+    should = require('should');
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    })
-  })
-})
+var stripeFire = require('../lib/stripe-fire.js')('private key');
+
+describe('StripeFire', function () {
+
+    describe('charges', function () {
+        it('should exist', function () {
+            (typeof stripeFire.charges !== 'undefined').should.be.true;
+        });
+
+        it('should be a function', function () {
+            (stripeFire.charges).should.be.a.Function;
+        });
+    });
+
+    describe('customers', function () {
+        it('should exist', function () {
+            (typeof stripeFire.customers !== 'undefined').should.be.true;
+        });
+
+        it('should be a function', function () {
+            (stripeFire.customers).should.be.a.Function;
+        });
+
+        var customers = new stripeFire.customers(new Firebase('https://stripe-fire.firebaseio.com/customers'));
+
+        describe('cards', function () {
+            it('should exist', function () {
+                (typeof customers.cards !== 'undefined').should.be.true;
+            });
+
+            it('should be a function', function () {
+                (customers.cards).should.be.a.Function;
+            });
+        });
+    });
+});

f7e64b392c2f05c03a88ed1388f377272d041e85
Dave <davidcaseria@gmail.com>
2014-09-27 15:58:32 -0500

    add charges functionality

--- test/stripe-fire.js
+++ test/stripe-fire.js
@@ -1,17 +1,44 @@
 var Firebase = require('firebase'),
-    should = require('should');
-
-var stripeFire = require('../lib/stripe-fire.js')('private key');
+    should = require('should'),
+    stripe = require('stripe')('sk_test_BQokikJOvBiI2HlWgH4olfQ2');
 
 describe('StripeFire', function () {
 
+    var stripeFire = require('../lib/stripe-fire.js')('sk_test_BQokikJOvBiI2HlWgH4olfQ2');
+
     describe('charges', function () {
+        before(function (done) {
+            stripe.tokens.create({
+                card: {
+                    number: '4242424242424242',
+                    exp_month: 12,
+                    exp_year: 2015,
+                    cvc: '123'
+                }
+            }, function (err, token) {
+                var ref = new Firebase('https://stripe-fire.firebaseio.com/charges');
+                ref.set({}, function () {
+                    ref.push({
+                        amount: 400,
+                        currency: 'usd',
+                        card: token.id
+                    }, function () {
+                        done();
+                    });
+                });
+            });
+        });
+
         it('should exist', function () {
             (typeof stripeFire.charges !== 'undefined').should.be.true;
         });
 
         it('should be a function', function () {
             (stripeFire.charges).should.be.a.Function;
+        });
+
+        it('should process a charge', function (done) {
+            stripeFire.charges('https://stripe-fire.firebaseio.com/charges', null, null, done);
         });
     });
 
@@ -22,18 +49,6 @@ describe('StripeFire', function () {
 
         it('should be a function', function () {
             (stripeFire.customers).should.be.a.Function;
-        });
-
-        var customers = new stripeFire.customers(new Firebase('https://stripe-fire.firebaseio.com/customers'));
-
-        describe('cards', function () {
-            it('should exist', function () {
-                (typeof customers.cards !== 'undefined').should.be.true;
-            });
-
-            it('should be a function', function () {
-                (customers.cards).should.be.a.Function;
-            });
         });
     });
 });

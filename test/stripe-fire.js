746aafa037178cc05eee4f14e53672f66b01d55f
Dave <davidcaseria@gmail.com>
2014-09-28 20:11:20 -0500

    finished charges test, started customers refactoring

--- test/stripe-fire.js
+++ test/stripe-fire.js
@@ -45,6 +45,32 @@ describe('StripeFire', function () {
 
         describe('refunds', function () {
 
+            before(function (done) {
+                stripe.tokens.create({
+                    card: {
+                        number: '4242424242424242',
+                        exp_month: 12,
+                        exp_year: 2015,
+                        cvc: '123'
+                    }
+                }, function (err, token) {
+                    var ref = new Firebase('https://stripe-fire.firebaseio.com/charges-refunds');
+                    ref.set({}, function () {
+                        var chargeRef = ref.child('charges').push({
+                            amount: 400,
+                            currency: 'usd',
+                            card: token.id
+                        }, function () {
+                            ref.child('refunds').child(chargeRef.name()).set({
+                                amount: 400
+                            }, function () {
+                                done();
+                            });
+                        });
+                    });
+                });
+            });
+
             it('should exist', function () {
                 (typeof charges.refunds !== 'undefined').should.be.true;
             });
@@ -53,11 +79,26 @@ describe('StripeFire', function () {
                 (charges.refunds).should.be.a.Function;
             });
 
-            /*
-            it('should process a refund', function (done) {
-                charges.refunds('https://stripe-fire.firebaseio.com/refunds', done);
+            it('should refund an existing charge', function (done) {
+                var charges = stripeFire.charges('https://stripe-fire.firebaseio.com/charges-refunds/charges', function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    charges.refunds('https://stripe-fire.firebaseio.com/charges-refunds/refunds', done);
+                });
             });
-            */
         });
+    });
+
+    describe('customers', function () {
+
+        it('should exist', function () {
+            (typeof stripeFire.customers !== 'undefined').should.be.true;
+        });
+
+        it('should be a function', function () {
+            (stripeFire.customers).should.be.a.Function;
+        });
+
     });
 });
